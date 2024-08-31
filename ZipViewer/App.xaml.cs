1783c62f8bbebf46624a86848b37e187e2f16926
Novik Oleksandr <novikoleksandr9@gmail.com>
2024-08-31 20:04:29 +0300

    Add project files.

---
+++ ZipViewer/App.xaml.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.UI.Xaml;
+using ZipViewer.ViewModels;
+using ZipViewer.Views;
+
+namespace ZipViewer;
+
+public partial class App : Application
+{
+    public IHost Host
+    {
+        get;
+    }
+
+    public static T GetService<T>()
+        where T : class
+    {
+        if ((App.Current as App)!.Host.Services.GetService(typeof(T)) is not T service)
+        {
+            throw new ArgumentException($"{typeof(T)} needs to be registered in ConfigureServices within App.xaml.cs.");
+        }
+
+        return service;
+    }
+
+    public static WindowEx MainWindow { get; } = new MainWindow();
+
+    public static UIElement? AppTitlebar
+    {
+        get; set;
+    }
+
+    public App()
+    {
+        InitializeComponent();
+
+        Host = Microsoft.Extensions.Hosting.Host.
+        CreateDefaultBuilder().
+        UseContentRoot(AppContext.BaseDirectory).
+        ConfigureServices((context, services) =>
+        {
+            // Views and ViewModels
+            services.AddTransient<MainViewModel>();
+            services.AddTransient<MainPage>();
+
+            // Configuration
+        }).
+        Build();
+    }
+
+    /// <summary>
+    /// Invoked when the application is launched.
+    /// </summary>
+    /// <param name="args">Details about the launch request and process.</param>
+    protected override void OnLaunched(LaunchActivatedEventArgs args)
+    {
+        m_window = MainWindow;
+        m_window.Content = GetService<MainPage>();
+        m_window.Activate();
+    }
+
+    private Window m_window;
+}
---
+++ ZipViewer/MainWindow.xaml.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.UI.Dispatching;
+using Windows.UI.ViewManagement;
+using ZipViewer.Helpers;
+
+namespace ZipViewer;
+
+public sealed partial class MainWindow : WindowEx
+{
+    private readonly DispatcherQueue dispatcherQueue;
+
+    private readonly UISettings settings;
+
+    public MainWindow()
+    {
+        InitializeComponent();
+
+        AppWindow.SetIcon(Path.Combine(AppContext.BaseDirectory, "Assets/WindowIcon.ico"));
+        Title = "Zip Viewer";
+
+        dispatcherQueue = DispatcherQueue.GetForCurrentThread();
+        settings = new UISettings();
+        settings.ColorValuesChanged += OnSettingsColorValuesChanged;
+    }
+
+    /// <summary>
+    /// This handles updating the caption button colors correctly when Windows system theme is changed
+    /// while the app is open
+    /// </summary>
+    private void OnSettingsColorValuesChanged(UISettings sender, object args)
+    {
+        dispatcherQueue.TryEnqueue(() =>
+        {
+            TitleBarHelper.ApplySystemThemeToCaptionButtons();
+        });
+    }
+}
