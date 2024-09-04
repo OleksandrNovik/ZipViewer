a1d0da4f1554a27a22ee579dedc9817163277dd5
Novik Oleksandr <novikoleksandr9@gmail.com>
2024-09-04 21:38:23 +0300

    Added converter for date time offset

---
+++ ZipViewer/UI/Converters/DateOffsetToFormatConverter.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.UI.Xaml.Data;
+
+namespace ZipViewer.UI.Converters;
+
+public sealed class DateOffsetToFormatConverter : IValueConverter
+{
+    public string Format
+    {
+        get;
+        set;
+    }
+    public object Convert(object value, Type targetType, object parameter, string language)
+    {
+        var str = string.Empty;
+
+        if (value is DateTimeOffset date)
+        {
+            str = date.ToString(Format);
+        }
+
+        return str;
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, string language) => throw new NotImplementedException();
+}
